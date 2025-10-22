f90f19d32a18c0b2a61dc156cec7818c4b1de2b0
Anthony <v81d@proton.me>
2025-10-22 14:02:21 -0400

    feat: create pet card generator

--- planetarium/src/main.ts
+++ planetarium/src/main.ts
@@ -1,11 +1,21 @@
+import type { StringMap } from "./scripts/typeDefs.ts";
 import planets from "./assets/json/planets.json";
+import setupPetSelector from "./scripts/petSelector";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+const container: HTMLDivElement = document.querySelector("#app")!;
+const availablePlanets: StringMap[] = planets as StringMap[];
+
+container.innerHTML = `
   <div>
     <h1 class="text-5xl font-bold">Select a Planet</h1>
-    <div class=""></div>
+    <div class="pet-selector"></div>
     <p class="text-xl">
       Select a planet as your pet.
     </p>
   </div>
 `;
+
+setupPetSelector(
+  container.querySelector<HTMLDivElement>(".pet-selector")!,
+  availablePlanets
+);
