d93ed15a992600dfd36cfc6b4510655b5fb79b3d
v81d <v81d@proton.me>
2025-10-24 12:09:00 -0400

    refactor: let petSelector handle starryBackground

--- planetarium/src/main.ts
+++ planetarium/src/main.ts
@@ -1,11 +1,6 @@
 import petsArray from "./assets/json/planets.json";
 import { appContainer } from "./scripts/globals.ts";
-import StarryBackground from "./scripts/interfaces/starryBackground.ts";
 import PetSelector from "./scripts/interfaces/petSelector.ts";
-
-// Render background
-let starryBackground = new StarryBackground(appContainer);
-starryBackground.generateBackground(50);
 
 // Pet selector interface
 let petSelector = new PetSelector(appContainer, petsArray);
