0c6d9975040de6f6f6da58e9afcb2b34462babb5
Anthony <v81d@proton.me>
2025-10-27 14:02:15 -0400

    feat: add pet selection callback

--- planetarium/src/main.ts
+++ planetarium/src/main.ts
@@ -9,3 +9,24 @@ let dotBackground = new DotBackground(appContainer);
 let petSelector = new PetSelector(appContainer, dotBackground, petsArray);
 petSelector.setupInterface();
 petSelector.insertPets();
+
+// Return the object for a pet given its ID, with the `intro` key omitted
+function fetchPet(petID: number) {
+  return (({ intro, ...object }) => object)(petsArray[petID]);
+}
+
+/* This function asynchronously (in order to not block the main thread) checks if `petSelector.done` is `true`.
+ * If `true`, then continue/begin the game.
+ */
+async function pollPetSelectorDone() {
+  while (!petSelector.done) {
+    await new Promise((resolve) => setTimeout(resolve, 100));
+  }
+  console.log(
+    `The user selected a pet. The selected pet is: ${JSON.stringify(
+      fetchPet(petSelector.selectedPetID)
+    )}`
+  );
+}
+
+pollPetSelectorDone();
