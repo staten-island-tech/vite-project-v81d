348fbeeaa886396a64d0e1e908936b28f8fdae44
v81d <v81d@proton.me>
2025-10-30 21:54:11 -0400

    feat: add panel to game interface

--- planetarium/src/scripts/interfaces/gameView.ts
+++ planetarium/src/scripts/interfaces/gameView.ts
@@ -1,21 +1,36 @@
-/* This class is used to construct the framework for the actual base interface of the game.
- * Event listeners are handled elsewhere
- */
-
+// This class is used to construct the framework for the actual base interface of the game
 export default class GameInterface {
   #appContainer: HTMLDivElement;
+  #themeSwitcher: any;
   #gameInterface?: HTMLDivElement;
+  #gameInterfaceTopPanel?: HTMLDivElement;
+  #themeSwitcherButton?: HTMLButtonElement;
 
-  constructor(appContainer: HTMLDivElement) {
+  constructor(appContainer: HTMLDivElement, themeSwitcher: any) {
     this.#appContainer = appContainer;
+    this.#themeSwitcher = themeSwitcher;
   }
 
   build() {
+    let theme = localStorage.getItem("theme") ?? "dark-theme";
+
     this.#appContainer.insertAdjacentHTML(
       "afterbegin",
       `
     <div class="app__game-interface">
-      <h1 class="text-3xl font-bold">GAME CONTENT PLACEHOLDER</h1>
+      <div class="game-interface__top-panel">
+        <div class="top-panel__labels">
+          <h1 class="text-2xl font-bold">planetarium</h1>
+          <h2>The Observatory</h2>
+        </div>
+        <div class="top-panel__buttons">
+          <ul>
+            <li>
+              <button id="theme-switcher-panel-button" class="top-panel__button">${theme === "dark-theme" ? "Light Theme" : "Dark Theme"}</button>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
     `,
     );
@@ -24,6 +39,13 @@ export default class GameInterface {
       ".app__game-interface",
     ) as HTMLDivElement;
     this.#gameInterface.offsetHeight; // reflow
+    this.#gameInterfaceTopPanel = this.#gameInterface.querySelector(
+      ".game-interface__top-panel",
+    ) as HTMLDivElement;
+    this.#themeSwitcherButton = this.#gameInterfaceTopPanel.querySelector(
+      "#theme-switcher-panel-button",
+    ) as HTMLButtonElement;
+    this.#themeSwitcher.attachClickAction(this.#themeSwitcherButton);
   }
 
   fadeIn() {
